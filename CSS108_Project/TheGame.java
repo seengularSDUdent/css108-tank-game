a30a9b4aeccb4c22f730d9fa823e7fede24f6698
seengularSDUdent <71064801+seengularSDUdent@users.noreply.github.com>
2021-04-13 23:45:01 +0600

    Add files via upload

--- CSS108_Project/TheGame.java
+++ CSS108_Project/TheGame.java
@@ -5,11 +5,19 @@ public class TheGame {
   public static void main(String[] args) throws Exception{
     java.io.File file = new java.io.File("map.txt");
     Scanner input = new Scanner(file);
-/*  Map map = new Map(input);
-    map.print();
-    System.out.println(map.getValueAt(map.getX()/2, map.getY()/2)); */
+    Map map = new Map(input);
 
-    
+    map.print();
+
+    Game tankGame = new Game(map);
+    Player newPlayer = new MyPlayer();
+
+    tankGame.addPlayer(newPlayer);
+//    newPlayer.setMap(map);
+    System.out.println(newPlayer.getPosition());
+    newPlayer.moveLeft();
+    newPlayer.moveLeft();
+    System.out.println(newPlayer.getPosition());
   }
 }
 
@@ -119,5 +127,143 @@ class Map{
 class InvalidMapException extends Exception{
   InvalidMapException(String message){
     super(message);
+  }
+}
+
+class Game {
+
+  Map gameMap;
+  Player gamePlayer;
+
+  public Game(Map theMapForGame){
+    gameMap = theMapForGame;
+  }
+
+  public void setMap(Map theMapForGameSet){
+    gameMap = theMapForGameSet;
+  }
+
+  public void addPlayer(Player thePlayerForGameAdd){
+    gamePlayer = thePlayerForGameAdd;
+    gamePlayer.setMap(gameMap);
+  }
+}
+
+interface Player{
+  public void setMap(Map theMapForPlayerSet);
+  public void moveRight();
+  public void moveLeft();
+  public void moveUp();
+  public void moveDown();
+  public Position getPosition();
+}
+
+class MyPlayer implements Player{
+
+  private Map currentMap;
+  private Position currentPosition;
+  private int playerDirection = 5;
+
+  @Override
+    public void setMap(Map theMapForPlayerSet){
+     this.currentMap = theMapForPlayerSet;
+     System.out.print(currentMap.getX() + " " + currentMap.getY() + " ");
+      for(int i = 0; i < currentMap.getY(); i++){
+        for(int j = 0; j < currentMap.getX(); j++){
+          if(currentMap.getValueAt(j, i) == 'P'){
+            System.out.println(currentMap.getValueAt(j, i));
+            currentPosition = new Position(j, i);
+            break;
+          }
+        }
+      }
+      System.out.println();
+    }
+
+  @Override
+    public void moveRight(){
+      if((currentMap.getX() !=  currentPosition.getX() + 1) && (currentMap.getValueAt(currentPosition.getX() + 1, currentPosition.getY()) == '0') && playerDirection == 6){
+        currentPosition.setX(currentPosition.getX() + 1);
+      }
+      else{
+        playerDirection = 6;
+      }
+    }
+
+  @Override
+    public void moveLeft(){
+      if((currentPosition.getX() != 0) && (currentMap.getValueAt(currentPosition.getX() - 1, currentPosition.getY()) == '0') && playerDirection == 4){
+        currentPosition.setX(currentPosition.getX() - 1);
+      }
+      else{
+        playerDirection = 4;
+      }
+    }
+
+  @Override
+    public void moveUp(){
+      if((currentPosition.getY() != 0) && (currentMap.getValueAt(currentPosition.getX(), currentPosition.getY() - 1) == '0') && playerDirection == 8){
+        currentPosition.setY(currentPosition.getY() - 1);
+      }
+      else{
+        playerDirection = 8;
+      }
+    }
+
+  @Override
+    public void moveDown(){
+      if((currentMap.getY() != currentPosition.getY() + 1) && (currentMap.getValueAt(currentPosition.getX(), currentPosition.getY() + 1) == '0') && playerDirection == 2){
+        currentPosition.setY(currentPosition.getY() + 1);
+      }
+      else{
+        playerDirection = 2;
+      }
+    }
+
+  @Override
+    public Position getPosition(){
+      return currentPosition;
+    }
+}
+
+class Position{
+
+  int mainX, mainY;
+
+  public Position(int theX, int theY){
+    this.mainX = theX;
+    this.mainY = theY;
+  }
+
+  public void setX(int theXForPositionSet){
+    this.mainX = theXForPositionSet;
+  }
+
+  public void setY(int theYForPositionSet){
+    this.mainY = theYForPositionSet;
+  }
+
+  public int getX(){
+    return this.mainX;
+  }
+
+  public int getY(){
+    return this.mainY;
+  }
+
+  public boolean equals(Position thePositionForTest){
+    if(this.getX() == thePositionForTest.getX()){
+      if(this.getY() == thePositionForTest.getY()){
+        return true;
+      }
+      else{
+        return false;
+      }
+    }
+    return false;
+  }
+
+  public String toString(){
+    return "X is " + this.getX() + " , Y is " + this.getY() + ".";
   }
 }
